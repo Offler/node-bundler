5783caf4a3f6c17955489664547a74bbdfa98b10
Brian Di Palma <offler@gmail.com>
2013-03-03 14:07:52 +0000

    Adding comments to JSClass spec file.

--- spec/bundler/resources/js/JSClass.spec.js
+++ spec/bundler/resources/js/JSClass.spec.js
@@ -3,10 +3,11 @@ var JSClass = require('../../../../lib/bundler/resources/js/JSClass').JSClass;
 describe('A JSClass', function(){
 	'use strict';
 	
-	it('will return the file location it was constructed with', function(){
-		
+	it('will return the file location it was constructed with.', function(){
+		//Given.
 		var jsClass = new JSClass('TestClass.js', 'bundler/test/TestClass.js');
 		
+		//Should.
 		expect(jsClass.getFileLocation()).toEqual('bundler/test/TestClass.js');
 	});
 });
