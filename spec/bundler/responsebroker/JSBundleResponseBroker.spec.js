6d714a7c80a1bc27dec59850fa2239576f06c081
Brian Di Palma <offler@gmail.com>
2013-03-05 20:50:12 +0000

    Added witeNamespaceObjectLiteral method to JSBundleResponseBroker class and added jasmine test for new method. Export constructor in more classes.

---
+++ spec/bundler/responsebroker/JSBundleResponseBroker.spec.js
@@ -0,0 +1,32 @@
+var JsMockito = require('jsmockito').JsMockito;
+var http = require('http');
+
+var JSBundleResponseBroker = require('../../../lib/bundler/responsebroker/JSBundleResponseBroker');
+
+describe('A JSBundleResponseBroker', function(){
+	'use strict';
+	
+	JsMockito.Integration.Nodeunit();
+	
+	var serverResponse = {};
+	var jsBundleResponseBroker = {};
+	
+	beforeEach(function(){
+		var mockCreatorHelper = new MockCreatorHelper();
+		
+		serverResponse = mock(mockCreatorHelper.createServerResponse());
+		
+		jsBundleResponseBroker = new JSBundleResponseBroker(serverResponse);
+	});
+	
+	it('will write the namespace object literal when given it.', function(){
+		//Given.
+		var namespaceObjectLiteral = 'toplevel = {subpackage : {}, secondsubpackage : {}};';
+		
+		//Doing.
+		jsBundleResponseBroker.writeNamespaceObjectLiteral(namespaceObjectLiteral);
+		
+		//Should.
+		verify(serverResponse).write(namespaceObjectLiteral);
+	});
+});
